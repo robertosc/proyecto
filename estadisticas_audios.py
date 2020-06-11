6f11597617b2e54921840126084637253440ba77
robertosc <robert.s.c30@gmail.com>
2020-06-10 23:37:19 -0600

    arreglos

---
+++ estadisticas_audios.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import sys
+
+def tiempo_total(lista_tiempos, lista_hablantes, index, dict_total):
+	'''
+	Calcula el tiempo total que habla cada persona del programa
+	'''
+	total = 0
+	for i in range(len(lista_tiempos)):
+		total+=lista_tiempos[i]
+	dict_total[lista_hablantes[index]] = total
+
+def promedio(lista_tiempos, lista_hablantes, index ,dict_estadisticas):
+	'''
+	Calcula el promedio que tardan las intervenciones de cada persona
+	'''
+	total = 0
+	for i in range(len(lista_tiempos)):
+		total+=lista_tiempos[i]
+	promedio = total/len(lista_tiempos)
+	dict_estadisticas[lista_hablantes[index]] = promedio
+
+def hablantes(df):
+	'''
+	Genera con todos los hablantes del programa
+	'''
+	lista_hablantes = []
+	for i in range(len(df)):
+		if df['data2'][i] not in lista_hablantes:
+			lista_hablantes.append(df['data2'][i])
+	return lista_hablantes
+
+def dict_promedio_todos(df, lista_hablantes):
+
+	estadisticas = {}
+
+	for j in range(len(lista_hablantes)):
+		lista_tiempos = []
+		for k in range(len(df)):
+			if df['data2'][k] == lista_hablantes[j]:
+				tiempo_habla = -df['data0'][k] + df['data1'][k]
+				lista_tiempos.append(tiempo_habla)
+		promedio(lista_tiempos, lista_hablantes, j, estadisticas)
+	return estadisticas
+
+def dict_total_todos(df, lista_hablantes):
+	total = {}
+
+	for j in range(len(lista_hablantes)):
+		lista_tiempos = []
+		for k in range(len(df)):
+			if df['data2'][k] == lista_hablantes[j]:
+				tiempo_habla = -df['data0'][k] + df['data1'][k]
+				lista_tiempos.append(tiempo_habla)
+		tiempo_total(lista_tiempos, lista_hablantes, j, total) #Añade elementos a total
+	return total
+
+def genero(df, lista_hablantes):
+	'''
+	Genera una lista con los hablantes hombres y hablantes mujeres.
+	'''
+	hombres = []
+	mujeres = []
+	tiempos_h = []
+	tiempos_m = []
+	for i in range(len(lista_hablantes)-1):
+		if lista_hablantes[i][len(lista_hablantes[i])-1] == "M" or lista_hablantes[i][len(lista_hablantes[i])-1] == "m":
+			mujeres.append(lista_hablantes[i])
+		elif lista_hablantes[i][len(lista_hablantes[i])-1] == "H" or lista_hablantes[i][len(lista_hablantes[i])-1] == "h":
+			hombres.append(lista_hablantes[i])
+		for j in range(len(df)-1):
+			if df['data2'][i] in hombres:
+				tiempos_h.append(df['data1'][j] - df['data0'][j])
+			else:
+				tiempos_m.append(df['data1'][j] - df['data0'][j])
+
+	suma_h = sum(tiempos_h)
+	suma_m = sum(tiempos_m)
+	prom_hombres = (suma_h*100) / (suma_h + suma_m)
+	prom_mujeres = (suma_m*100) / (suma_h + suma_m)
+	results = {"Hombres" : prom_hombres, "Mujeres" :  prom_mujeres}
+	return results
+
+def multiples_archivos(argv):
+	for i in range(1, len(sys.argv)):
+		filename = sys.argv[i]
+		df = pd.read_csv(filename, sep='\t', engine='python', header=None, prefix="data")
+		lista_hablantes = hablantes(df)
+		print("Archivo: ", sys.argv[i], "\n\nTiempo promedio de habla:\n", dict_promedio_todos(df,lista_hablantes), "\n\nTiempo total del habla:\n", dict_total_todos(df, lista_hablantes),  "\n\n")
+		print("Hombres y mujeres:" , genero(df, lista_hablantes))
+
+
+
+def main(argv):
+	multiples_archivos(argv)
+
+
+#filename = "/mnt/c/Users/rober/OneDrive/Documentos/20200312_desayunos_parte2_estadistica.segments"
+#df = pd.read_csv(filename, sep='\t', engine='python')
+
+main(sys.argv)
+#genero(df, hablantes(df))
